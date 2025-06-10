7e189c5d37924dbda66e5cf3825cb7a43ea7fe8f
heshamabdelhamid <heshamabdelhamid432@gmail.com>
2025-06-10 13:55:20 +0300

    finish episode-13

--- src/app.jsx
+++
@@ -1,5 +0,0 @@
-const tech = "React, Tailwind and Vite";
-
-export function App() {
-  return <h1 className="font-mono text-4xl">Hello {tech}!</h1>;
-}
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
 })
